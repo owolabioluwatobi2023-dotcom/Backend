7add158cff471fdfb485efa21aff6fbf870b3ff3
Owolabi Oluwatobi <owolabioluwatobi2023@gmail.com>
2026-07-11 14:19:33 -0700

    fix firebase credentials for render

--- notifications/firebase.py
+++ notifications/firebase.py
@@ -21,55 +21,73 @@
 #     firebase_admin.initialize_app(cred)
 
 
-import firebase_admin
-from firebase_admin import credentials, messaging
+# import firebase_admin
+# from firebase_admin import credentials, messaging
+# import os
+
+
+# BASE_DIR = os.path.dirname(
+#     os.path.dirname(
+#         os.path.abspath(__file__)
+#     )
+# )
+
+
+# firebase_key = os.path.join(
+#     BASE_DIR,
+#     "firebase-service-account.json"
+# )
+
+
+# if not firebase_admin._apps:
+#     cred = credentials.Certificate(firebase_key)
+#     firebase_admin.initialize_app(cred)
+
+
+
+# def send_push_notification(user, title, body):
+
+#     from .models import DeviceToken
+
+#     tokens = DeviceToken.objects.filter(
+#         user=user
+#     )
+
+
+#     for device in tokens:
+
+#         message = messaging.Message(
+
+#             notification=messaging.Notification(
+#                 title=title,
+#                 body=body
+#             ),
+
+#             token=device.token
+
+#         )
+
+
+#         try:
+#             messaging.send(message)
+
+#         except Exception as e:
+#             print("Firebase error:", e)
+
+
 import os
-
-
-BASE_DIR = os.path.dirname(
-    os.path.dirname(
-        os.path.abspath(__file__)
-    )
-)
-
-
-firebase_key = os.path.join(
-    BASE_DIR,
-    "firebase-service-account.json"
-)
+import json
+import firebase_admin
+from firebase_admin import credentials
 
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(firebase_key)
-    firebase_admin.initialize_app(cred)
 
+    firebase_json = os.environ.get("FIREBASE_CREDENTIALS")
 
-
-def send_push_notification(user, title, body):
-
-    from .models import DeviceToken
-
-    tokens = DeviceToken.objects.filter(
-        user=user
-    )
-
-
-    for device in tokens:
-
-        message = messaging.Message(
-
-            notification=messaging.Notification(
-                title=title,
-                body=body
-            ),
-
-            token=device.token
-
+    if firebase_json:
+        cred = credentials.Certificate(
+            json.loads(firebase_json)
         )
 
-
-        try:
-            messaging.send(message)
-
-        except Exception as e:
-            print("Firebase error:", e)
+        firebase_admin.initialize_app(cred)
